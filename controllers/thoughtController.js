bf646774dab96f9a6356099cf7e095bd957d1dbd
sevaggap <sevag.gaprielian@gmail.com>
2022-03-18 16:57:45 -0400

    created structe for neede functions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,38 @@
 const { User, Thought } = require('../models');
 
-module.exports = {};
+module.exports = {
+    // get all the thoughts
+    getThoughts(req,res) {
+
+    },
+
+    // get one thought 
+    getSingleThought(req,res) {
+
+    },
+
+    // create a new thought
+    createThought(req,res) {
+
+    },
+    
+    // update an existing thought
+    updateThought(req,res) {
+
+    },
+
+    // delete an existing thought
+    deleteThought(req,res) {
+
+    },
+
+    // add a reaction
+    addReaction(req,res) {
+
+    },
+
+    // delete a reaction
+    deleteReaction(req,res) {
+
+    }
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,38 @@
 const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all the users
+    getUsers(req,res) {
+
+    },
+
+    // get one user 
+    getSingleUser(req,res) {
+
+    },
+
+    // create a new user
+    createUser(req,res) {
+
+    },
+    
+    // update an existing user 
+    updateUser(req,res) {
+
+    },
+
+    // delete an existing user
+    deleteUser(req,res) {
+
+    },
+
+    // add a friend
+    addFriend(req,res) {
+
+    },
+
+    // delete a friend
+    deleteFriend(req,res) {
+
+    }
+};
