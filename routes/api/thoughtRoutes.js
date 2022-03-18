6741093647dd93399d1c9522877fd0315d27bcc4
sevaggap <sevag.gaprielian@gmail.com>
2022-03-18 16:49:08 -0400

    wrote basic route defenitions in the thoughtRoutes and userRoutes files

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 
+router.route('/').get().post();
+
+router.route('/:thoughtid').get().put().delete()
+
+router.route('/:thoughtid/reactions/').post().delete()
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 
+router.route('/').get().post();
+
+router.route('/:userid').get().put().delete()
+
+router.route('/:userid/friends/:friendid').post().delete()
 
 module.exports = router;
