e7b07663a654f7cd449ed506bb3fd8eee0b02d76
sevaggap <sevag.gaprielian@gmail.com>
2022-03-18 17:14:11 -0400

    updated index.js file to create the server, imported the controllers into the routes and assigned routes their respective functions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
 
-router.route('/').get().post();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+  } = require('../../controllers/thoughtController');
 
-router.route('/:thoughtid').get().put().delete()
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtid/reactions/').post().delete()
+router.route('/:thoughtid').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtid/reactions/').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+  } = require('../../controllers/userController.js');
 
-router.route('/').get().post();
+router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userid').get().put().delete()
+router.route('/:userid').get(getSingleUser).put(updateUser).delete(deleteUser)
 
-router.route('/:userid/friends/:friendid').post().delete()
+router.route('/:userid/friends/:friendid').post(addFriend).delete(deleteFriend)
 
 module.exports = router;
